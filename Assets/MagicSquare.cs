0c202c2bec4cc9562e05da7df4099d37023e2bdf
Tomohiko Mukai <contact@mukai-lab.org>
2023-05-11 11:07:37 +0900

    revised numbers

--- Assets/MagicSquare.cs
+++ Assets/MagicSquare.cs
@@ -13,26 +13,26 @@ public class MagicSquare : MonoBehaviour
         magic[0, 2] = -1;
         magic[0, 3] = 5;
         magic[0, 4] = 19;
-        magic[1, 0] = 7;
+        magic[1, 0] = 3;
         magic[1, 1] = -1;
-        magic[1, 2] = -1;
-        magic[1, 3] = 5;
-        magic[1, 4] = 19;
-        magic[2, 0] = 7;
-        magic[2, 1] = -1;
-        magic[2, 2] = -1;
-        magic[2, 3] = 5;
-        magic[2, 4] = 19;
-        magic[3, 0] = 7;
-        magic[3, 1] = -1;
+        magic[1, 2] = 9;
+        magic[1, 3] = -1;
+        magic[1, 4] = -1;
+        magic[2, 0] = 14;
+        magic[2, 1] = 22;
+        magic[2, 2] = 1;
+        magic[2, 3] = 18;
+        magic[2, 4] = -1;
+        magic[3, 0] = -1;
+        magic[3, 1] = 8;
         magic[3, 2] = -1;
-        magic[3, 3] = 5;
-        magic[3, 4] = 19;
-        magic[4, 0] = 7;
-        magic[4, 1] = -1;
-        magic[4, 2] = -1;
-        magic[4, 3] = 5;
-        magic[4, 4] = 19;
+        magic[3, 3] = -1;
+        magic[3, 4] = -1;
+        magic[4, 0] = -1;
+        magic[4, 1] = 4;
+        magic[4, 2] = 17;
+        magic[4, 3] = 6;
+        magic[4, 4] = 13;
     }
 
     // Update is called once per frame
